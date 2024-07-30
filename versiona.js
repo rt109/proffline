bf7bffe00a62cf53b63be5ab72c10024ad7be55d
rt109 <rt109@qq.com>
2024-07-30 10:11:34 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,3 +1,3 @@
 var lastVersion = 3;
 var downUrl = "https://github.com/rt109/proffline/raw/main/updateapp/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E8%82%89%E9%B8%BD%E5%A6%96%E6%A2%A61.03.apk";
-var updateLog = "禁用了商店里按住确定的操作（请不要狂按确定了），修复下1波按上后崩溃bug";
+var updateLog = "1：再次修复商店。2：增加了控制台组件，可以用来看到报错，能够拖动。3：同步最新版。新模式，起名，选人过滤，增加技能动画音乐等等。";
