0955625c19fc355e31e99084d2fd7a0e208f141c
rt109 <rt109@qq.com>
2024-07-30 09:24:50 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-var lastVersion = 1;
+var lastVersion = 4;
 var downUrl = "https://github.com/rt109/proffline/raw/main/updateapp/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E8%82%89%E9%B8%BD%E5%A6%96%E6%A2%A61.03.apk";
 var updateLog = "禁用了商店里按住确定的操作（请不要狂按确定了），修复下1波按上后崩溃bug";
