c50d1bbfd547322830a023baf9e2430f1c94cc43
rt109 <rt109@qq.com>
2024-07-30 08:44:47 +0800

    Update versionp.js

--- versionp.js
+++ versionp.js
@@ -1,4 +1,4 @@
 var lastVersion = 4;
 var downUrl = "https://rt109.github.io/proffline/update/updateV1.0.1.zip";
 var updateLog = "1：再次修复商店。2：增加了控制台组件，可以用来看到报错，能够拖动。3：同步最新版。新模式，起名，选人过滤，增加技能动画音乐等等。";
-var fileName = "宝可梦肉鸽妖梦1.04.zip";
+var fileName = "全平台宝可梦肉鸽妖梦1.04百度云.zip";
