ac03e80f0e6a4420a9b8eaa041089a194d3b2dd3
rt109 <rt109@qq.com>
2024-07-30 15:27:20 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
-var lastVersion = 3;
+var lastVersion = 4;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp.txt";
 var updateLog = "1：增加了控制台控件，可以用来看到报错。2：妖梦模式修复商店，下调第1个劲敌强度，上调闪光率，更换部分奖励。3：同步最新版。新模式，起名，选人过滤，增加技能动画音乐等等。\n！！！更新前请导出存档并移动到非游戏目录！！！";
 var fileName = "宝可梦肉鸽妖梦V1.04.apk";
