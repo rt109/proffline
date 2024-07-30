9be66444e6997a1242a5bca375ce16d112da44df
rt109 <rt109@qq.com>
2024-07-30 12:51:38 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,3 +1,4 @@
 var lastVersion = 3;
-var downUrl = "https://github.com/rt109/proffline/raw/main/updateapp/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E8%82%89%E9%B8%BD%E5%A6%96%E6%A2%A61.03.apk";
-var updateLog = "1：再次修复商店。2：增加了控制台组件，可以用来看到报错，能够拖动。3：同步最新版。新模式，起名，选人过滤，增加技能动画音乐等等。";
+var downUrl = "https://rt109.github.io/proffline/update/updateV1.0.4.zip";
+var updateLog = "1：增加了控制台控件，可以用来看到报错。2：妖梦模式修复商店，下调第1个劲敌强度，上调闪光率，更换部分奖励。3：同步最新版。新模式，起名，选人过滤，增加技能动画音乐等等。";
+var fileName = "宝可梦肉鸽妖梦V1.04.apk";
