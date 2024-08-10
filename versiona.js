49bdd33b06131d5371960c16a25fb4d36e4a3fbd
rt109 <rt109@qq.com>
2024-08-10 22:37:57 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
 var lastVersion = 5;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.05.txt";
-var updateLog = "主要提高了手机体验 1。调整查bug的创建顺序，1开始就创建 2。增加放大缩小画面功能 3。增加调整键位功能 4。增加备份存档功能，在导出菜单和按返回键时能备份 5。开始界面增加浏览存档位置功能 6。增加导出自定义名字存档功能 7。修复没有检测到是手机而没按键的bug 8。修复长按失灵bug";
+var updateLog = "v1.05:主要提高了手机体验 1。调整查bug的创建顺序，1开始就创建 2。增加放大缩小画面功能 3。增加调整键位功能 4。增加备份存档功能，在导出菜单和按返回键时能备份 5。开始界面增加浏览存档位置功能 6。增加导出自定义名字存档功能 7。修复没有检测到是手机而没按键的bug 8。修复长按失灵bug";
 var fileName = "宝可梦肉鸽妖梦V1.05.apk";
