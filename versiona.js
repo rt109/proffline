9e68f7f84a24f6607e0866f83d49165acdb0015b
rt109 <rt109@qq.com>
2024-09-16 21:39:53 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
-var lastVersion = 7;
+var lastVersion = 9;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.06.txt";
-var updateLog = "V1.08  一。修复动画文件读取bug  二。口令窗口换回旧版，口令和1.07一样  三。增加训练师改昵称功能，在数据管理那里  四。科比模式更新等等";
-var fileName = "宝可梦肉鸽妖梦V1.08.apk";
+var updateLog = "V1.09  1。修复闪光立绘丢失  2。下调科比门槛为11114   3。增加蛋券掉落up活动   4。同步最新版本：历史记录功能，跳过孵蛋动画，平衡调整，实现修复招式等等";
+var fileName = "宝可梦肉鸽妖梦V1.09.apk";
