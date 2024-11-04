93cd41969355cf8040e561b074dca2fa0ba3595f
rt109 <rt109@qq.com>
2024-11-04 11:40:26 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
-var lastVersion = 9;
+var lastVersion = 11;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.06.txt";
-var updateLog = "V1.09  1。修复闪光立绘丢失  2。下调科比门槛为11114   3。增加蛋券掉落up活动   4。同步最新版本：历史记录功能，跳过孵蛋动画，平衡调整，实现修复招式等等";
-var fileName = "宝可梦肉鸽妖梦V1.09.apk";
+var updateLog = "V1.11：1。增加1只神秘新宠。旧档会提示你更新数据格式，确定就行。2。新宠和回音能从玛纳霏蛋出货 3。妖梦模式上调劲敌奖励，下调训练师等级，商店商品和刷新费用独立，根据幸运值降价 4。妖梦和科比模式增加神秘遭遇，曼巴精神越高遭遇机率越大 5。蛋券掉落口令bug修复，原先1.10版本是无效的QAQ，1.09才有 6。同步最新版，万圣节活动、闪光变种，事件宝可梦训练师改动等等";
+var fileName = "宝可梦肉鸽妖梦V1.11.apk";
