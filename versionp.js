06f5538d990744821e8a0009e66b07f7e5004e74
rt109 <rt109@qq.com>
2024-11-04 11:45:33 +0800

    Update versionp.js

--- versionp.js
+++ versionp.js
@@ -1,4 +1,4 @@
-var lastVersion = 8;
-var downUrl = "https://rt109.github.io/proffline/update/updateV1.07~1.08.zip";
-var updateLog = "V1.08:  一。修复动画文件读取bug  二。口令窗口换回旧版，口令和1.07一样  三。增加训练师改昵称功能，在数据管理那里  四。科比模式更新等等";
-var fileName = "updateV1.07~1.08.zip";
+var lastVersion = 11;
+var downUrl = "https://rt109.github.io/proffline/update/1.11.zip";
+var updateLog = "V1.11：1。增加1只神秘新宠。旧档会提示你更新数据格式，确定就行。2。新宠和回音能从玛纳霏蛋出货 3。妖梦模式上调劲敌奖励，下调训练师等级，商店商品和刷新费用独立，根据幸运值降价 4。妖梦和科比模式增加神秘遭遇，曼巴精神越高遭遇机率越大 5。蛋券掉落口令bug修复，原先1.10版本是无效的QAQ，1.09才有 6。同步最新版，万圣节活动、闪光变种，事件宝可梦训练师改动等等";
+var fileName = "1.11.zip";
