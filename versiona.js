acf6553515cd7f106b6003718ba56d116af74036
rt109 <rt109@qq.com>
2025-02-04 10:22:27 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
-var lastVersion = 12;
+var lastVersion = 14;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.06.txt";
-var updateLog = "V1.12：  1。增加1只小奇怪新宠。旧档会提示你更新数据格式，确定就行。  2。修复新宠闪光bug  3。工具条增加还原默认设置按钮，再也不怕手贱按键丢失  4。同步最新版，会心一投、技能蛋招特性被动的实装与平衡改动等等";
-var fileName = "宝可梦肉鸽妖梦V1.12.apk";
+var updateLog = "V1.14 1。增加了新年奇树（进化分支） 2。修复妖梦劲敌奖励过多bug 3。增加了蛇年活动 4。同步最新版，反转能力挑战，随机选人，初始宝可梦调整等等";
+var fileName = "宝可梦肉鸽妖梦V1.14.apk";
