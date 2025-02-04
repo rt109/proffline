fa1ba569c44e29998d5e226bd4657a646049550b
rt109 <rt109@qq.com>
2025-02-04 10:29:08 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
 var lastVersion = 14;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.06.txt";
-var updateLog = "V1.14 1。增加了新年奇树（进化分支） 2。修复妖梦劲敌奖励过多bug 3。增加了蛇年活动 4。同步最新版，反转能力挑战，随机选人，初始宝可梦调整等等";
+var updateLog = "V1.14： 1。增加了新年奇树（进化分支） 2。修复妖梦劲敌奖励过多bug 3。增加了蛇年活动 4。同步最新版，反转能力挑战，随机选人，初始宝可梦调整等等";
 var fileName = "宝可梦肉鸽妖梦V1.14.apk";
