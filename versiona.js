802ac37d3f400941622cb3c74aedad535e728fad
rt109 <rt109@qq.com>
2025-03-22 15:36:38 +0800

    Update versiona.js

--- versiona.js
+++ versiona.js
@@ -1,4 +1,4 @@
-var lastVersion = 14;
+var lastVersion = 15;
 var downUrl = "https://raw.githubusercontent.com/rt109/proffline/main/updateapp/prapp1.06.txt";
-var updateLog = "V1.14： 1。增加了新年奇树（进化分支） 2。修复妖梦劲敌奖励过多bug 3。增加了蛇年活动 4。同步最新版，反转能力挑战，随机选人，初始宝可梦调整等等";
-var fileName = "宝可梦肉鸽妖梦V1.14.apk";
+var updateLog = "V1.15： 1。妖梦劲敌和机制： https://www.bilibili.com/video/BV17mXkYxEZm 2。修复科比模式通关结算bug，闪光和昵称显示bug 3。蛇年活动换成宝可梦日活动 4。同步最新版，菜单图鉴，太晶化改版，蛋招被动调整等等";
+var fileName = "宝可梦肉鸽妖梦V1.15.apk";
